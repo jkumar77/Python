676ae6932f92da41b778dda493e229cea796ce0f
Jeevan Kumar <jeevan.kaushal1984@gmail.com>
2021-07-31 08:52:58 +0530

    Fetched tags from dictionary

--- get_aws_s3_tags.py
+++ get_aws_s3_tags.py
@@ -8,7 +8,7 @@ print(session.region_name)
 s3_client = session.client('s3')
 for bucket in s3_client.list_buckets()['Buckets']:
     TAG = s3_client.get_bucket_tagging(Bucket=bucket['Name'])
-    print(bucket['Name'], TAG['TagSet'])
+    print(bucket['Name'], TAG['TagSet'][0]['Value'])
 
 
 
